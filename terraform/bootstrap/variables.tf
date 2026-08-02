48b9d711394595a51c51c17a4132721b09f3f050
DaNaHiju <dnhijuelos@gmail.com>
2026-08-02 20:34:06 +0300

    bootstrap-terraform

---
+++ terraform/bootstrap/variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  description = "Project name used to build the state bucket and lock table names"
+  type        = string
+  default     = "ecommerce-api"
+}
+
+variable "aws_region" {
+  description = "AWS region for the backend resources (S3 bucket + DynamoDB table)"
+  type        = string
+  default     = "us-east-1"
+}
